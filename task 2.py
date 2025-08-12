67baa4fc18a4ff230cbd40eb624efd3010f0d7ae
rohab171 <nithishrohan171@gmail.com>
2025-08-12 18:28:21 +0530

    task2

---
+++ task 2.py
@@ -0,0 +1,32 @@
+import math
+
+mother_name="vanitha"
+father_name="murali"
+brother_name="niash"
+print("my_mother_name_is:",mother_name)
+print("my father_name_is:",father_name)
+print("my bro name:",brother_name)
+
+my_twelth_mark_list="acc=70,eco=80,tam=70,eng=69"
+print(my_twelth_mark_list)
+my_tenth_score="eng=88,tam=70,mat=50,sci=66,soc=70"
+print(my_tenth_score)
+a=5
+a**=2
+print(a)
+l=4
+b=9
+A=l*b
+print(A)
+h=8
+B=1/2*(b*h)
+print(B)
+d=5
+e=6
+C=((d+e)*h)/2
+print(C)
+r=6
+print(math.pi*(r**2))
+
+
+
