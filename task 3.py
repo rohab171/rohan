0219a5916d106c4f385be922cc05d5218986b24f
rohab171 <nithishrohan171@gmail.com>
2025-08-13 17:36:37 +0530

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,22 @@
+name = "rohan"
+print(name)
+a=5
+b=7
+c=8.9
+d="6h+7xy"
+e=True
+print(a+b)
+print(a<b)
+print(a>b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(type(a))
+print(type(d))
+print(type(c))
+print(type(e))
+
+
+
+
